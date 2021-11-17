53179cac2171cd81efe8dc964e57f5e58c837b8a
Алексей Довыденков <51017158+alex16541@users.noreply.github.com>
2021-11-17 19:14:09 +0300

    App height on mobile fix 3.0

--- js/pages/app/windowHeightResize.js
+++ js/pages/app/windowHeightResize.js
@@ -1,3 +1,15 @@
+var body = document.documentElement;
+if (body.requestFullscreen) {
+body.requestFullscreen();
+} else if (body.webkitrequestFullscreen) {
+body.webkitrequestFullscreen();
+} else if (body.mozrequestFullscreen) {
+body.mozrequestFullscreen();
+} else if (body.msrequestFullscreen) {
+body.msrequestFullscreen();
+}
+
+
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 // Мы прослушиваем событие resize
