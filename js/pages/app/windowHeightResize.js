c185396b0156220af3d1f0261b3f4e8655665f4c
Алексей Довыденков <51017158+alex16541@users.noreply.github.com>
2021-11-17 18:46:26 +0300

    App height on mobile fix 2.0

--- js/pages/app/windowHeightResize.js
+++ js/pages/app/windowHeightResize.js
@@ -1,3 +1,5 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 // Мы прослушиваем событие resize
 window.addEventListener('resize', () => {
   // Выполняем тот же скрипт, что и раньше
